fd6d61f33a6dbda23d7ee904298fdfdca11f43be
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-17 11:21:08 +0600

    compiled project

---
+++ target/2.2.0/photon/obj/src/newlib_stubs.o.d
@@ -0,0 +1,149 @@
+/Users/bakytdzhumabaev/particle_workbench/four_leds/target/2.2.0/photon//obj/src/newlib_stubs.o: \
+ src/newlib_stubs.cpp \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/types.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/newlib-nano/newlib.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_newlib_version.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/config.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/ieeefp.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/features.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/cdefs.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_default_types.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_types.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_types.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/lock.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_stdint.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/endian.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_endian.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/select.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_sigset.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timeval.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/timespec.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timespec.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/sched.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/types.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/stdlib.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/cstdlib \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7-m/nofp/bits/c++config.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7-m/nofp/bits/os_defines.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7-m/nofp/bits/cpu_defines.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdlib.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/reent.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/stdlib.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/alloca.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/xlocale.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/bits/std_abs.h \
+ ../../../services/inc/debug.h ../../../services/inc/service_debug.h \
+ ../../../services/inc/logging.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/string.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/strings.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/string.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdint.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_intsup.h \
+ /Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ ../../../services/inc/panic.h ../../../services/inc/panic_codes.h \
+ ../../../services/inc/config.h ../../../services/inc/preprocessor.h
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/types.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/newlib-nano/newlib.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_newlib_version.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/config.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/ieeefp.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/features.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/cdefs.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_default_types.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_types.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_types.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/lock.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_stdint.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/endian.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_endian.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/select.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_sigset.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timeval.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/timespec.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timespec.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/sched.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/types.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/stdlib.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/cstdlib:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7-m/nofp/bits/c++config.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7-m/nofp/bits/os_defines.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7-m/nofp/bits/cpu_defines.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdlib.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/reent.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/stdlib.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/alloca.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/xlocale.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/bits/std_abs.h:
+
+../../../services/inc/debug.h:
+
+../../../services/inc/service_debug.h:
+
+../../../services/inc/logging.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/string.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/strings.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/string.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdint.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_intsup.h:
+
+/Users/bakytdzhumabaev/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h:
+
+../../../services/inc/panic.h:
+
+../../../services/inc/panic_codes.h:
+
+../../../services/inc/config.h:
+
+../../../services/inc/preprocessor.h:
---
+++ target/2.2.0/photon/obj/src/user_module.o.d
@@ -0,0 +1,4 @@
+/Users/bakytdzhumabaev/particle_workbench/four_leds/target/2.2.0/photon//obj/src/user_module.o: \
+ src/user_module.c ../../../dynalib/inc/dynalib.h
+
+../../../dynalib/inc/dynalib.h:
