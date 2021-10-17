bb3479e2863e9becf26752c9ef062ffa74e28b74
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-17 11:23:34 +0600

    rgb contrrol blue

--- src/four_leds.ino
+++ src/four_leds.ino
@@ -4,11 +4,14 @@
  * Author:
  * Date:
  */
-
+ const char* serviceUuid = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
+ const char* red         = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
+ const char* green       = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
+ const char* blue        = "6E400004-B5A3-F393-E0A9-E50E24DCCA9E";
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
-
+ RGB.control(true);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
